0c61a782c67ffefa08ce256a1dd155c464f1a904
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-09 13:30:24 +0100

    chore: eslint was disabled by mistake

--- src/changelog.ts
+++ src/changelog.ts
@@ -2,11 +2,11 @@ export function readChangeLog(content: string, version: string) {
     const lines = content.split("\n");
     const result: string[] = [];
     let inVersion = false;
-    const regex = /^(?:\s*)###?\s*\[?([0-9A-Zb-z_\-\.]+)/;
+    const regex = /^(?:\s*)###?\s*\[?([0-9A-Zb-z_\-.]+)/;
     for (const line of lines) {
         const versionResult = regex.exec(line);
         if (versionResult) {
-            if (versionResult[1].indexOf(".") >= 0) {
+            if (versionResult[1].includes(".")) {
                 inVersion = versionResult[1] === version;
                 continue;
             }
